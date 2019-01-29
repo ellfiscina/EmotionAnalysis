f64f32681409eb6d75f2ff9b6a28bd69acc9cc37
ellfiscina <ellfiscina@gmail.com>
2019-01-29 15:45:47 -0300

    remove debug

--- tcc/settings.py
+++ tcc/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@5)6w)h@5pr^$w)gb61cq%30u$du+od7-vjp*7z%rh(sm=#c*%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['10.87.155.93']
 
